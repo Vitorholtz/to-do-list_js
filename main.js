ab18b13ba77fda3074ec00dad36f402f743eacee
Vítor Holtz <71574549+Vitorholtz@users.noreply.github.com>
2021-01-25 15:02:21 -0300

    port

---
+++ main.js
@@ -0,0 +1,55 @@
+// selectors
+
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+//event listeners
+
+todoButton.addEventListener ('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
+
+//Functions 
+
+function addTodo(event) {
+
+event.preventDefault()
+
+ // create to-do div
+
+ const todoDiv = document.createElement('div');
+ todoDiv.classList.add('todo');
+
+ // create list
+ const newTodo = document.createElement('li');
+ newTodo.innerText = todoInput.value;
+ newTodo.classList.add('todo-item');
+ todoDiv.appendChild(newTodo);
+
+ // check mark button
+ const completedButton = document.createElement ('button');
+ completedButton.innerHTML = '<i class = "fas fa-check"> </i>';
+ completedButton.classList.add('complete-btn');
+ todoDiv.appendChild(completedButton);
+
+  //  trash button
+  const trashButton = document.createElement ('button');
+  trashButton.innerHTML = '<i class = "fas fa-trash"> </i>';
+  trashButton.classList.add('trash-btn');
+  todoDiv.appendChild(trashButton);
+
+  //apend to list
+  todoList.appendChild(todoDiv);
+
+  // clear input value
+  todoInput.value = '';
+
+}
+
+function deleteCheck() {
+
+    const item = e.target;
+    
+
+    
+}
